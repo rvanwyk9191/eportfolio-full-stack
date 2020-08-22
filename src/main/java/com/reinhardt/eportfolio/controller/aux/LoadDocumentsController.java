e4b1fb12a059fe328c2bc3f05ef30deee02d93f2
Reinhardt <rvanwyk9191@gmail.com>
2020-08-22 15:43:28 -0500

    Modifications to have an internal document finder to be able to deploy documents individually. This also includes a new Jenkinsfile_doc to handle these deployments

---
+++ src/main/java/com/reinhardt/eportfolio/controller/aux/LoadDocumentsController.java
@@ -0,0 +1,31 @@
+package com.reinhardt.eportfolio.controller.aux;
+
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.InputStreamResource;
+import org.springframework.core.io.PathResource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.io.IOException;
+
+@RestController
+public class LoadDocumentsController {
+
+    @GetMapping(value="/getDocument",produces = "application/pdf")
+    public ResponseEntity<InputStreamResource> getDocument(@RequestParam String documentName){
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("Content-Disposition", "inline; filename=" + documentName);
+        PathResource pdfFile = new PathResource("/documents/" + documentName);
+        ResponseEntity<InputStreamResource> response = null;
+        try {
+            response = new ResponseEntity<>(
+                    new InputStreamResource(pdfFile.getInputStream()), headers, HttpStatus.OK);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return response;
+    }
+
+}
